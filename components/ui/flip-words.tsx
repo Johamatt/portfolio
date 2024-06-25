38b45d258111381c36a59a5cd0249be713c05f70
johamatt94 <johamatt94@gmail.com>
2024-06-26 00:01:09 +0300

    fix text inline

--- components/ui/flip-words.tsx
+++ components/ui/flip-words.tsx
@@ -74,7 +74,7 @@ export const FlipWords = ({
               delay: index * 0.08,
               duration: 0.4,
             }}
-            className="inline-block text-[35px] text-cyan-500 mb-4 font-bold text-4xl"
+            className="inline-block text-cyan-500 mb-4 font-bold lg:text-4xl md:text-4xl text-3xl"
           >
             {letter}
           </motion.span>
